10d651481b0f1d341ec42bd9b5fa33928a1fa4a3
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-03-11 19:17:07 -0500

    Updated Tobey Scene

---
+++ Team Prototype 2/Assets/Scripts/CoffeeHit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeHit : MonoBehaviour
+{
+
+    public GameObject coffee;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(coffee)
+        {
+            Debug.Log("spill");
+            other.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
---
+++ Team Prototype 2/Assets/Scripts/TouchController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+
+    private bool dragActive = false;
+
+    Vector2 screenPosition;
+
+    Vector3 worldPosition;
+
+    public GameObject lastCoffee;
+
+
+    void Awake()
+    {
+        TouchController[] controllers = FindObjectsOfType<TouchController>();
+        if(controllers.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log(screenPosition);
+        //Debug.Log(worldPosition);
+
+        if (dragActive)
+        {
+            if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Drop();
+                return;
+            }
+        }
+        
+        if(Input.touchCount > 0)
+        {
+            screenPosition = Input.GetTouch(0).position;
+        }
+        else
+        {
+            return;
+        }
+
+        worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        if (dragActive)
+        {
+            Drag();
+        }
+        else
+        {
+            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+            if(hit.collider != null)
+            {
+                if(hit.collider.tag == "Coffee")
+                {
+                    Debug.Log("You hit Coffee");
+                }
+
+
+                GameObject coffee = hit.transform.gameObject;
+                if(coffee != null)
+                {
+                    lastCoffee = coffee;
+                    InitDrag();
+                }
+            }
+        }
+    }
+
+    void InitDrag()
+    {
+        dragActive = true;
+    }
+
+    void Drag()
+    {
+        lastCoffee.transform.position = new Vector2(worldPosition.x, worldPosition.y);
+    }
+
+    void Drop()
+    {
+        dragActive = false;
+    }
+}
