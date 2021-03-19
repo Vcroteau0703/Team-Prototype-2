12c7c03138203f9373a8c9deadee27f4a0aef8cd
Tom Parker <parkertom076@gmail.com>
2021-03-19 16:31:32 -0400

    Lunchbox Button and image

---
+++ Team Prototype 2/Assets/Scripts/SpawnTouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnTouch : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] Prefabs;
+
+    private int randomPrefab;
+    public Transform Spawn;
+    public Transform Canvas;
+
+    // Update is called once per frame
+    public void LunchBox() {
+        Debug.Log("Start");
+        randomPrefab = Random.Range(0, 5);
+        Debug.Log(randomPrefab);
+        Image FoodSpawn = Instantiate(Prefabs[randomPrefab], Spawn.position, Quaternion.identity) as Image;
+        FoodSpawn.transform.SetParent(Canvas.transform);
+
+        Debug.Log("instantiate object");
+    }
+}
