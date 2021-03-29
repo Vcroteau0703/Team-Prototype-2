f89c4919c7d0d4ccac0439ebbc0a165c3421ab5e
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-03-29 18:15:09 -0400

    Added automatic value resets to the temperature system, updated Tobey Scene

--- Team Prototype 2/Assets/Scripts/TemperatureSlider.cs
+++ Team Prototype 2/Assets/Scripts/TemperatureSlider.cs
@@ -5,95 +5,67 @@ using UnityEngine.UI;
 
 public class TemperatureSlider : MonoBehaviour
 {
-
     public float temperature;
     public Text temperatureUI;
 
     public float seatHeat;
     public int maxHeat = 100;
     public bool seatOn;
-    //public Text seatHeatUI;
-    public Slider seatHeatSlider;
 
+    public Slider seatHeatSlider;
     public Slider stressSlider;
 
-    public float timer;
+
+    float temperatureTimer;
+    float seatWarmerTimer;
 
     void Start()
     {
         SetMaxHeat(maxHeat);
+
+        temperatureTimer = 10;
+        seatWarmerTimer = 10;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        temperatureUI.text = temperature.ToString("0");
-
-        if(temperature > 80)
-        {
-            Debug.Log("TOO HOT!!!");
-            stressSlider.value += 1f;
-        } 
-        else if(temperature < 20)
-        {
-            Debug.Log("TOO COLD!!!");
-        }
-        else if (temperature < -20)
-        {
-            Debug.Log("I'M FREEZING!!!");
-        }
-        else if (temperature > 120)
-        {
-            Debug.Log("I'M MELTING!!!");
-        }
-
-
-        if (seatOn)
-        {
-            seatHeat += 5 * Time.deltaTime;
-        } 
-        else if (!seatOn && seatHeat > 0)
-        {
-            seatHeat -= 5 * Time.deltaTime;
-
-            if(seatHeat < 0)
-            {
-                seatHeat = 0;
-            }
-        }
-
-        if(seatHeat > 100)
-        {
-            Debug.Log("MY BUTT... IT BURNS!!!!");
-            seatHeat = maxHeat;
-        }
-
-        SetHeat(seatHeat);
+        TemperatureSystem();
+        SeatWarmerSystem();
     }
 
     public void TemperatureUp()
     {
         temperature += 1;
+
+        if (temperature > 120)
+        {
+            stressSlider.value += 1;
+        }
     }
 
     public void TemperatureDown()
     {
         temperature -= 1;
+
+        if (temperature < -120)
+        {
+            stressSlider.value += 1;
+        }
     }
 
-    
+
     public void SeatHeatPress()
     {
         if (seatOn)
         {
             seatOn = false;
-        } 
+        }
         else if (!seatOn)
         {
             seatOn = true;
         }
     }
-    
+
 
     public void SetMaxHeat(int heat)
     {
@@ -105,4 +77,120 @@ public class TemperatureSlider : MonoBehaviour
     {
         seatHeatSlider.value = heat;
     }
+
+    void TemperatureSystem()
+    {
+        temperatureUI.text = temperature.ToString("0");
+
+        if (temperature > 80)
+        {
+            Debug.Log("TOO HOT!!!");
+            stressSlider.value += 50 * Time.deltaTime;
+
+            if (temperatureTimer > 0)
+            {
+                temperatureTimer -= 1 * Time.deltaTime;
+            }
+
+
+            if (temperature > 80 && seatHeat > 80)
+            {
+                Debug.Log("I'M MELTING!!!");
+                stressSlider.value += 10 * Time.deltaTime;
+                //temperatureTimer -= 1 * Time.deltaTime;
+                //seatWarmerTimer -= 1 * Time.deltaTime;
+            }
+        }
+
+        if (temperature < 20)
+        {
+            Debug.Log("TOO COLD!!!");
+            stressSlider.value += 50 * Time.deltaTime;
+
+            if (temperatureTimer > 0)
+            {
+                temperatureTimer -= 1 * Time.deltaTime;
+            }
+
+            /*if (temperature < 20 && seatHeat > 80)
+            {
+                Debug.Log("I'M MELTING!!!");
+                stressSlider.value -= 40 * Time.deltaTime; //this goes down because the heat balances itself out
+            }*/
+        }
+
+        if (temperatureTimer <= 0)
+        {
+            if (temperature > 50)
+            {
+                temperature -= 1;
+                if (temperature < 50)
+                {
+                    temperature = 50;
+                    //temperatureTimer = 10;
+                }
+            }
+
+            if (temperature < 50)
+            {
+                temperature += 1;
+                if (temperature > 50)
+                {
+                    temperature = 50;
+                    //temperatureTimer = 10;
+                }
+            }
+
+            if (temperature == 50)
+            {
+                temperatureTimer = 10;
+            }
+        }
+    }
+
+    void SeatWarmerSystem()
+    {
+        if (seatOn)
+        {
+            seatHeat += 5 * Time.deltaTime;
+        }
+        else if (!seatOn && seatHeat > 0)
+        {
+            seatHeat -= 5 * Time.deltaTime;
+
+            if (seatHeat < 0)
+            {
+                seatHeat = 0;
+            }
+        }
+
+        if (seatHeat > 100)
+        {
+            Debug.Log("MY BUTT... IT BURNS!!!!");
+            stressSlider.value += 50 * Time.deltaTime;
+
+            if (seatWarmerTimer > 0)
+            {
+                seatWarmerTimer -= 1 * Time.deltaTime;
+            }
+
+            seatHeat = maxHeat;
+        }
+
+        SetHeat(seatHeat);
+
+        if (seatWarmerTimer <= 0)
+        {
+            seatOn = false;
+            seatWarmerTimer = 0;
+
+            if (seatHeat == 0)
+            {
+                seatWarmerTimer = 10;
+            }
+
+        }
+    }
+
 }
+
